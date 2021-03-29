0368536e61c78615c86acd7e045b60e8041d380a
h-jun01 <jyun.0110@icloud.com>
2021-03-29 22:37:25 +0900

    add:added a note

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+// このデコレータでproviderとして認識される。
+// そして、controllerのconstructorからDIされる。
 @Injectable()
 export class AppService {
+  // メソッド名はなんでも良い。controllerで指定した名前と違っても良い。（同じにした方がわかりやすい）
   getHello(): string {
     return 'Hello World!';
   }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // expressでいう const app = express();
+  // create()の引数にはルートモジュールクラスを指定する
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8002);
 }
 bootstrap();
